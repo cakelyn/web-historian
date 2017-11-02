ad1a9f49575422b84fa4a528e312521439f00995
Katelyn Stamas <cakelyn@gmail.com>
2017-11-01 20:00:33 -0400

    got index.html to show up on local server

--- helpers/archive-helpers.js
+++ helpers/archive-helpers.js
@@ -12,7 +12,8 @@ var _ = require('underscore');
 exports.paths = {
   siteAssets: path.join(__dirname, '../web/public'),
   archivedSites: path.join(__dirname, '../archives/sites'),
-  list: path.join(__dirname, '../archives/sites.txt')
+  list: path.join(__dirname, '../archives/sites.txt'),
+  index: path.join(__dirname, '../web/public/index.html')
 };
 
 // Used for stubbing paths for tests, do not modify
@@ -25,17 +26,35 @@ exports.initialize = function(pathsObj) {
 // The following function names are provided to you to suggest how you might
 // modularize your code. Keep it clean!
 
-exports.readListOfUrls = function(callback) {
+exports.readListOfUrls = function() {
+  // should read URLs from sites.txt
+  fs.readFile(exports.paths.list, 'utf-8', function(err, data){
+    if(err) {
+      console.log('error', error);
+    }
+    console.log(data);
+    return data.split('\n');
+
+  });
 };
 
 exports.isUrlInList = function(url, callback) {
+  // should check if a URL is in the list
+  var urlList = exports.readListOfUrls();
+  if(urlList.includes(url)){
+    return true;
+  }
+  return false;
 };
 
 exports.addUrlToList = function(url, callback) {
+  // should add a URL to the list
 };
 
 exports.isUrlArchived = function(url, callback) {
+  // should check if a URL is archived
 };
 
 exports.downloadUrls = function(urls) {
+  // should download all pending URLs in the list
 };
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,53 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var helpers = require('./http-helpers.js');
+var fs = require('fs');
 // require more modules/folders here!
 
+var sendResponse = function(res, url, status) {
+  var status = status || 200;
+  res.writeHead(status, helpers.headers);
+  res.end(url);
+};
+
+var collectUrl = function(req, callback) {
+  var url = '';
+  req.on('data', function(chunk) {
+    url += chunk;
+  });
+  req.on('end', function() {
+    callback(url);
+  });
+};
+
+var actions = {
+  'GET': function(req, res) {
+    // return the content of index.html
+    // should return the content of a website from the archive
+    fs.readFile('./web/public/index.html', function(err, data) {
+      sendResponse(res, data);
+    });
+
+  },
+  'POST': function(req, res) {
+    // append submitted sites to sites.txt
+    collectUrl(req, function(url) {
+      // if data is not in sites.txt
+      // show loading.html
+      sendResponse(res, url.slice(4), 201);
+    });
+  },
+  'OPTIONS': function(req, res) {
+    sendResponse(res, null);
+  }
+};
+
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  var action = actions[req.method];
+
+  if (action) {
+    action(req, res);
+  } else {
+    sendResponse(res, '', 404);
+  }
 };
